0acd4f2f6ddf64d5303ffd4b7ec837fc7106a2d7
Soulaimane Zarria <53901834+ZSoulaimane@users.noreply.github.com>
2022-08-07 14:05:53 +0100

    Fix - fixing API path

--- src/server.ts
+++ src/server.ts
@@ -30,8 +30,8 @@ import {Router , Request, Response} from 'express'
 
   /**************************************************************************** */
 
-  app.get('filteredimage', async (req: Request, res: Response) =>{
-    const image_url  = req.query.image_url.tiString();
+  app.get('/filteredimage', async (req: Request, res: Response) =>{
+    const image_url  = req.query.image_url.toString();
 
     if(!image_url){
       res.status(400).send(('Image url is required'));
